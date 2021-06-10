c0532547bac1a727f2a26d366a7993e2931fe6ff
expandboard <iokloud.com@gmail.com>
2021-06-10 23:57:51 +0430

    Update test and packet arg

---
+++ example.js
@@ -0,0 +1,70 @@
+// const mongoPersistence = require("aedes-persistence-mongodb");
+const HTTP = require('./lib/http')
+const aedes = require('aedes')()
+var authBroker = require('@borokero/borokero-auth')
+const debug = require('debug')('borokero-iot-http')
+const mqemitter = require('mqemitter-mongodb')
+const mongoPersistence = require('aedes-persistence-mongodb')
+
+
+const options = {
+  mqtt: {
+    port: 1883
+  },
+  http: {
+    port: 3000
+  },
+  db: {
+    url: process.env.MONGO_URL,
+    // Optional ttl settings
+    ttl: {
+      packets: process.env.MONGO_TTL_PACKETS, // Number of seconds
+      subscriptions: process.env.MONGO_TTL_SUB
+    }
+  },
+  envAuth: {
+    auth: {
+      realm: process.env.REALM,
+      "auth-server-url": process.env.AUTH_SERVER_URL,
+      "ssl-required": process.env.SSL_REQUIRED,
+      resource: process.env.RESOURCE,
+      "public-client": process.env.PUBLIC_CLIENT,
+      "confidential-port": process.env.CONFIDENTIAL_PORT,
+    }
+  }
+}
+
+
+const authbroker = new authBroker(options.envAuth)
+aedes = require('aedes')({
+  mq: mqemitter({
+    url: options.db.url
+  }),
+  persistence: mongoPersistence({
+    url: options.db.url,
+    // Optional ttl settings
+    ttl: {
+      packets: options.db.ttl.packets, // Number of seconds
+      subscriptions: options.db.ttl.subscriptions
+    }
+  })
+})
+
+aedes.on('retained', function (topic, payload) {
+  debug(
+    'Topic: \x1b[33m" + topic + "\x1b[0m',
+    'updated to: ',
+    payload.toString()
+  )
+})
+
+const http = new HTTP({
+    brokero: aedes,
+    port: options.http.port,
+    authenticate: authbroker.authenticateWithAccessToken(),
+    authorizeGet: authbroker.authorizeSubscribe(),
+    authorizePut: authbroker.authorizePublish()
+  },
+  function (err, res) {
+    console.log(err)
+  })
